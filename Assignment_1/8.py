9a45c440f5ababaafc7bad1b621b526e8f5d1102
mohitkumarcusb <144983865+mohitkumarcusb@users.noreply.github.com>
2023-10-16 23:00:57 +0530

    Delete Assignment_1/8.py

--- Assignment_1/8.py
+++
@@ -1,9 +0,0 @@
-#8. Python program to find the sum of the digits of an integer using a while loop
-x=int(input("please enter number:"))
-summ=0
-while(x!=0):
-    digit=x%10
-    summ=summ+digit
-    x= x//10
-print("the sum of digits=",summ)
-
