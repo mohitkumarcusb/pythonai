c227982780f1643642a59428253a5f4c3fc62f43
mohitkumarcusb <144983865+mohitkumarcusb@users.noreply.github.com>
2023-10-16 22:57:40 +0530

    Delete Assignment_1/16.py

--- Assignment_1/16.py
+++
@@ -1,6 +0,0 @@
-list = []
-a = int(input('How many numbers you want to insert into list: '))
-for n in range(a):
-    x = int(input('Enter number '))
-    list.append(x)
-print("Sum of numbers in the given list is :", sum(list))
