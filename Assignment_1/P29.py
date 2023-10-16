36b7a74323334eef6b9aecae477fd519f6f5d09e
mohitkumarcusb <144983865+mohitkumarcusb@users.noreply.github.com>
2023-10-16 23:12:51 +0530

    Delete Assignment_1/P29.py

--- Assignment_1/P29.py
+++
@@ -1,9 +0,0 @@
-#29. Python program to print all the items in a dictionary. 
-# Input a dictionary
-
-thisdict = {"Name": "Mohit", "Age": 23, "City": "Gaya"}
-
-# Print all items in the dictionary
-
-for key, value in thisdict.items():
-    print(f"{key}: {value}")
