3b6748c1931e05467c876ab9dc0704a056751f68
mohitkumarcusb <144983865+mohitkumarcusb@users.noreply.github.com>
2023-10-16 14:30:27 +0530

    Add files via upload

---
+++ Assignment_1/1.py
@@ -0,0 +1,4 @@
+#convert temperature
+temp=float(input("enter the temperature in centigrade:-"))
+fahrenheit=(temp*9/5+32) # formula
+print("the fahrenheit temperature=",fahrenheit)
---
+++ Assignment_1/4.py
@@ -0,0 +1,9 @@
+#4Python program to find the circumference and area of a circle with a given radius.
+r=float(input("enter the radius of circle:-"))
+area=3.14*r**2
+circumference=2*3.14*r
+print("the area of circle is:-",area)
+print("the circumference of circle is:",circumference)
+
+
+  
