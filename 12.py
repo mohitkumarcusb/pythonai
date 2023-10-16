5b29b52e16702566697e9ba813a156fb349fc36d
mohitkumarcusb <144983865+mohitkumarcusb@users.noreply.github.com>
2023-10-16 23:17:39 +0530

    Delete 12.py

--- 12.py
+++
@@ -1,9 +0,0 @@
-#12. Python program to generate the prime numbers from 1 to N.
-
-num =int(input("Enter the range you want to generate the prime numbers: "))
-for n in range(2,num):
-    for i in range(2,n):
-        if(n%i==0):
-            break
-    else:
-        print(n)   
